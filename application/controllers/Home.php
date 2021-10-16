f9f24bd73411ae0b4eda39fa77ba6d3e678b64c1
adinoid <ali.fahruddin.046@gmail.com>
2021-10-16 11:51:15 +0700

    membuat template partial header untuk views Home daftar buku, penulis, penerbit agar lebih clean code

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,18 +15,21 @@ class Home extends CI_Controller{
 
     public function book(){
         $this->load->model('loader');
+        $data['title'] = 'Halaman Daftar Buku';
         $data['bookdetail'] = $this->loader->get_book();
         $this->load->view('home/booklist',$data);  
     }
 
     public function author(){
         $this->load->model('loader');
+        $data['title'] = 'Halaman Daftar Penulis';
         $data['authordetail'] = $this->loader->get_author();
         $this->load->view('home/authorlist',$data);
     }
 
     public function publisher(){
         $this->load->model('loader');
+        $data['title'] = 'Halaman Daftar Penerbit';
         $data['publishdetail'] = $this->loader->get_publish();
 
         $this->load->view('home/publisherlist',$data);
